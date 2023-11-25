15ee182865dd45685041c580a1797e85550a90e4
archan8322 <111737142+archan8322@users.noreply.github.com>
2023-11-26 02:37:18 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+function handleResponse(response) {
+    if (response === 'fine') {
+      window.location.href = 'fine.html';
+    } else if (response === 'notfine') {
+      window.location.href = 'notfine.html';
+    }
+  }
+  
+  function goToIndex() {
+    window.location.href = 'index.html';
+  }
+  
+
+  function goToBestFriend() {
+    window.location.href = 'bestfriend.html';
+  }
+  
+  function handleResponse2(response) {
+    if (response === 'yes') {
+      window.location.href = 'yes.html';
+    } else if (response === 'no') {
+      window.location.href = 'no.html';
+    }
+  }
+  function handleResponse3(response) {
+    if (response === 'okay' || response === 'okayweare') {
+      window.location.href = 'okay.html';
+    } else if (response === 'mylife') {
+      window.location.href = 'mylife.html';
+    }
+  }
+
+  function goToSure() {
+    window.location.href = 'sure.html';
+  }
+ 
+  function goToYesWait() {
+    window.location.href = 'yeswait.html';
+  }
